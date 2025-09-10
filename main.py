04cf46b4715ebe05b4b7afd1bd49967ebb0999c1
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-09-10 10:23:54 -0500

    Clase 12"

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ clients = 'pablo,ricardo,'
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the clients\'s list')
 
 def list_clients():
     global clients
@@ -18,10 +21,25 @@ def _add_comma():
     global clients
 
     clients += ','
+
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today')
+    print('[C]reate client')
+    print('[D]elete client')
+
     
 if __name__ == '__main__':
-    list_clients()
-
-    create_client('David')
+    _print_welcome()
     
-    list_clients()
+    command = input()
+
+    if command == 'C':
+        client_name = input('What is the client name?')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
