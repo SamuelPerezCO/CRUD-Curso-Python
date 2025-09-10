d049aedd1ea70b077e590106e117c7e29eb0970f
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-09-10 12:06:32 -0500

    Search Client

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ def update_client(client_name, update_client_name):
     else:
         print('Client is not in clients list')
 
+def search_client(client_name):
+    clients_lists = clients.split(',')
+
+    for client in clients_lists:
+        if client != client_name:
+            continue
+        else:
+            return True
+
 def delete_client(client_name):
     global clients
 
@@ -45,6 +54,7 @@ def _print_welcome():
     print('[C]reate client')
     print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
 
 def _get_client_name():
     return  input('What is the client name? ')
@@ -63,6 +73,14 @@ if __name__ == '__main__':
         client_name = _get_client_name()
         delete_client(client_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client is in client\'s list')
+        else:
+            print(f'The client: {client_name} is not in our client\'s list')
     elif command == 'U':
         client_name = _get_client_name()
         update_client_name = input('What is the update client name: ')
