f08c1d992ca71a86a5c7b70289a59c09878766f2
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-09-15 19:01:01 -0500

    Adding click

---
+++ platzi-ventas/setup.py
@@ -0,0 +1,14 @@
+from setup import setup
+
+setup(
+    name = 'pv',
+    version = '0.1',
+    py_modules = ['pv'],
+    install_requires = [
+        'Click',
+    ],
+    entry_points = '''
+        [console_scripts]
+        pv=pv:cli
+    '''
+)
